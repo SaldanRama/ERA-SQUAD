07c478cb14acc0c0a95a16f57345cb433ae6ab54
Minhajul Yusri Khairi <113837592+MinhajulKhairi@users.noreply.github.com>
2023-06-02 00:09:36 +0700

    feat: perbaiki TableObat

--- app/src/main/java/glucometer/scenes/TableObat.java
+++ app/src/main/java/glucometer/scenes/TableObat.java
@@ -1,51 +1,82 @@
 package glucometer.scenes;
 
-import glucometer.dataBase.DbGulaDarah;
-import glucometer.models.GulaDarah;
+import java.sql.SQLException;
+
+import glucometer.dataBase.DbObat;
 import glucometer.models.Obat;
-import glucometer.models.Obat;
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class TableObat extends Scene {
-    private Stage stage;
-    private static ObservableList<Obat> obatList;
+    private static ObservableList<Obat> obatList = FXCollections.observableArrayList();
+    // private VBox rightSide = new VBox();
 
-    public static ObservableList<Obat> getObatList() {
-        return obatList;
-    }
 
-    public static void setTekananDarahList(ObservableList<Obat> obatList) {
-        TableObat.obatList = obatList;
-    }
-
-    public TableObat(Stage stage, ObservableList<Obat> obatList) {
+    public TableObat(Stage stage) {
         super(new VBox(), 480, 480);
-        this.stage = stage;
-        this.obatList = obatList;
+        DbObat daoObat = new DbObat();
+        try {
+            obatList.addAll(daoObat.getAll());
+            System.out.println(obatList.size());
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
 
         // Membuat tampilan scene
         VBox root = new VBox();
         root.setSpacing(10);
         root.setPadding(new Insets(10));
 
-        Label titleLabel = new Label("Tekanan Darah");
+        Label titleLabel = new Label("Obat");
         titleLabel.setStyle("-fx-font-size: 18px; -fx-font-weight: bold;");
-
-       
 
         Button tambahButton = new Button("Tambah");
         tambahButton.setOnAction(event -> {
-            SceneObat scObat = new SceneObat(stage, SceneObat.getObatList());
+            SceneObat scObat = new SceneObat(stage, obatList);
             stage.setScene(scObat);
         });
+
+        TableView<Obat> tableObat = new TableView<>();
+        // MembuAT Table Coloumn
+        TableColumn<Obat, String> coloumn1 = new TableColumn<>("Nama Obat");
+        TableColumn<Obat, Integer> coloumn2 = new TableColumn<>("Dosis");
+        TableColumn<Obat, String> coloumn3 = new TableColumn<>("Bentuk");
+        TableColumn<Obat, String> coloumn4 = new TableColumn<>("Catatan");
+        TableColumn<Obat, String> coloumn5 = new TableColumn<>("Tanggal");
+
+        // Pasangkan
+        coloumn1.setCellValueFactory(new PropertyValueFactory<>("namaObat"));
+        coloumn2.setCellValueFactory(new PropertyValueFactory<>("dosis"));
+        coloumn3.setCellValueFactory(new PropertyValueFactory<>("bentuk"));
+        coloumn4.setCellValueFactory(new PropertyValueFactory<>("catatan"));
+        coloumn5.setCellValueFactory(new PropertyValueFactory<>("tanggal"));
+
+        // tambah colum ke table
+        tableObat.getColumns().addAll(coloumn1, coloumn2, coloumn3, coloumn4, coloumn5);
+
+        // Kasi nilai
+        tableObat.setItems(obatList);
+
+        TextField tfNamaObat = new TextField();
+        tfNamaObat.setPromptText("Nama Obat");
+        TextField tfDosis = new TextField();
+        tfDosis.setPromptText("Dosis");
+        TextField tfBentuk = new TextField();
+        tfBentuk.setPromptText("Bentuk");
+        TextField tfCatatan = new TextField();
+        tfCatatan.setPromptText("Catatan");
+        TextField tfTanggal = new TextField();
+        tfTanggal.setPromptText("Tanggal");
 
         Button kembaliButton = new Button("Kembali");
         kembaliButton.setOnAction(v -> {
@@ -53,7 +84,7 @@ public class TableObat extends Scene {
             mainScene.show();
         });
 
-        root.getChildren().addAll(titleLabel, tambahButton, kembaliButton);
+        root.getChildren().addAll(titleLabel, tambahButton, kembaliButton, tableObat);
 
         setRoot(root);
     }
