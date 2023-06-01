88e66bce7a084d60f24ed8e69873baa86dd45e63
Minhajul Yusri Khairi <113837592+MinhajulKhairi@users.noreply.github.com>
2023-06-02 00:03:52 +0700

    feat: update terbaru SceneBeratBadan

--- app/src/main/java/glucometer/scenes/SceneBeratBadan.java
+++ app/src/main/java/glucometer/scenes/SceneBeratBadan.java
@@ -1,44 +1,21 @@
 package glucometer.scenes;
 
 import glucometer.dataBase.DbBeratBadan;
-import glucometer.dataBase.DbGulaDarah;
 import glucometer.models.BeratBadan;
-import glucometer.models.GulaDarah;
 import javafx.collections.ObservableList;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class SceneBeratBadan extends Scene {
-    private Stage stage;
-    private static ObservableList<BeratBadan> beratBadanList;
-
-   
-    public Stage getStage() {
-        return stage;
-    }
-
-    public void setStage(Stage stage) {
-        this.stage = stage;
-    }
-
-    public static ObservableList<BeratBadan> getBeratBadanList() {
-        return beratBadanList;
-    }
-
-    public static void setBeratBadanList(ObservableList<BeratBadan> beratBadanList) {
-        SceneBeratBadan.beratBadanList = beratBadanList;
-    }
+    // private static ObservableList<BeratBadan> beratBadanList;
 
     public SceneBeratBadan(Stage stage, ObservableList<BeratBadan> beratBadanList) {
         super(new VBox(), 480, 480);
-        this.stage = stage;
-        this.beratBadanList = beratBadanList;
 
         // Membuat tampilan scene
         VBox root = new VBox();
@@ -52,13 +29,16 @@ public class SceneBeratBadan extends Scene {
         beratBadanTextField.setPromptText("Berat Badan");
         TextField catatanTextField = new TextField();
         catatanTextField.setPromptText("Tambah Catatan Disini");
+        TextField tanggalTextField = new TextField();
+        tanggalTextField.setPromptText("Tanggal");
 
         Button tambahButton = new Button("Tambah");
         tambahButton.setOnAction(event -> {
             int beratBadan = Integer.parseInt(beratBadanTextField.getText());
             String catatan = catatanTextField.getText();
+            String tanggal = tanggalTextField.getText();
 
-            BeratBadan beratBadanObj = new BeratBadan(beratBadan, catatan);
+            BeratBadan beratBadanObj = new BeratBadan(beratBadan, catatan, tanggal);
             beratBadanList.add(beratBadanObj);
 
             // Simpan ke database (TO DO LIST 1)
@@ -68,6 +48,7 @@ public class SceneBeratBadan extends Scene {
             // Clear input fields
             beratBadanTextField.clear();
             catatanTextField.clear();
+            tanggalTextField.clear();
         });
 
         Button kembaliButton = new Button("Kembali");
@@ -76,7 +57,8 @@ public class SceneBeratBadan extends Scene {
             mainScene.show();
         });
 
-        root.getChildren().addAll(titleLabel, beratBadanTextField, catatanTextField, tambahButton, kembaliButton);
+        root.getChildren().addAll(titleLabel, beratBadanTextField, catatanTextField, tanggalTextField, tambahButton,
+                kembaliButton);
 
         setRoot(root);
     }
