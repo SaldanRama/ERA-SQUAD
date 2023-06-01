dd1443c162cc99a620ed27aaba41c8db4a9e047d
Minhajul Yusri Khairi <113837592+MinhajulKhairi@users.noreply.github.com>
2023-06-01 23:59:35 +0700

    feat: tambah variabel tanggal

--- app/src/main/java/glucometer/models/BeratBadan.java
+++ app/src/main/java/glucometer/models/BeratBadan.java
@@ -3,10 +3,12 @@ package glucometer.models;
 public class BeratBadan {
     private int beratBadan;
     private String catatan;
-    
-    public BeratBadan(int beratBadan, String catatan) {
+    private String tanggal;
+
+    public BeratBadan(int beratBadan, String catatan, String tanggal) {
         this.beratBadan = beratBadan;
         this.catatan = catatan;
+        this.tanggal = tanggal;
     }
     
     public int getBeratBadan() {
@@ -21,6 +23,16 @@ public class BeratBadan {
     public void setCatatan(String catatan) {
         this.catatan = catatan;
     }
+    public String getTanggal() {
+        return tanggal;
+    }
+    public void setTanggal(String tanggal) {
+        this.tanggal = tanggal;
+    }
+    
+  
+    
+ 
     
 }
 
