11bae689841964446eb77501a06ff047a63d1793
Minhajul Yusri Khairi <113837592+MinhajulKhairi@users.noreply.github.com>
2023-06-02 00:10:13 +0700

    feat: perbaiki TableTekananDarah

--- app/src/main/java/glucometer/scenes/TableTekananDarah.java
+++ app/src/main/java/glucometer/scenes/TableTekananDarah.java
@@ -1,40 +1,24 @@
 package glucometer.scenes;
 
-<<<<<<< HEAD
 import java.sql.SQLException;
 
-import glucometer.dataBase.DbGulaDarah;
 import glucometer.dataBase.DbTekananDarah;
-import glucometer.models.GulaDarah;
 import glucometer.models.TekananDarah;
 import javafx.collections.FXCollections;
-=======
-import glucometer.dataBase.DbGulaDarah;
-import glucometer.models.GulaDarah;
-import glucometer.models.TekananDarah;
->>>>>>> c3644c56205a812715fa598d2b583cf597bd2a3d
 import javafx.collections.ObservableList;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
-<<<<<<< HEAD
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
-=======
-import javafx.scene.control.TextField;
->>>>>>> c3644c56205a812715fa598d2b583cf597bd2a3d
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class TableTekananDarah extends Scene {
-<<<<<<< HEAD
     private static ObservableList<TekananDarah> tekananDarahList = FXCollections.observableArrayList();
-    private VBox rightSide = new VBox();
-
 
     public TableTekananDarah(Stage stage) {
         super(new VBox(), 480, 480);
@@ -45,23 +29,6 @@ public class TableTekananDarah extends Scene {
         } catch (SQLException e) {
             e.printStackTrace();
         }
-=======
-    private Stage stage;
-    private static ObservableList<TekananDarah> tekananDarahList;
-
-    public static ObservableList<TekananDarah> getTekananDarahList() {
-        return tekananDarahList;
-    }
-
-    public static void setTekananDarahList(ObservableList<TekananDarah> tekananDarahList) {
-        TableTekananDarah.tekananDarahList = tekananDarahList;
-    }
-
-    public TableTekananDarah(Stage stage, ObservableList<TekananDarah> tekananDarahList) {
-        super(new VBox(), 480, 480);
-        this.stage = stage;
-        this.tekananDarahList = tekananDarahList;
->>>>>>> c3644c56205a812715fa598d2b583cf597bd2a3d
 
         // Membuat tampilan scene
         VBox root = new VBox();
@@ -71,7 +38,6 @@ public class TableTekananDarah extends Scene {
         Label titleLabel = new Label("Tekanan Darah");
         titleLabel.setStyle("-fx-font-size: 18px; -fx-font-weight: bold;");
 
-<<<<<<< HEAD
         Button tambahButton = new Button("Tambah");
         tambahButton.setOnAction(event -> {
             SceneTekananDarah scTekananDarah = new SceneTekananDarah(stage, tekananDarahList);
@@ -85,17 +51,17 @@ public class TableTekananDarah extends Scene {
         TableColumn<TekananDarah, Integer> coloumn2 = new TableColumn<>("Tekanan Diastolik");
         TableColumn<TekananDarah, String> coloumn3 = new TableColumn<>("Tangan");
         TableColumn<TekananDarah, String> coloumn4 = new TableColumn<>("Catatan");
-
+        TableColumn<TekananDarah, String> coloumn5 = new TableColumn<>("Tanggal");
 
         // Pasangkan
         coloumn1.setCellValueFactory(new PropertyValueFactory<>("tekananSistolik"));
         coloumn2.setCellValueFactory(new PropertyValueFactory<>("tekananDiastolik"));
         coloumn3.setCellValueFactory(new PropertyValueFactory<>("tangan"));
         coloumn4.setCellValueFactory(new PropertyValueFactory<>("catatan"));
-
+        coloumn5.setCellValueFactory(new PropertyValueFactory<>("tanggal"));
 
         // tambah colum ke table
-        tableTekananDarah.getColumns().addAll(coloumn1, coloumn2, coloumn3, coloumn4);
+        tableTekananDarah.getColumns().addAll(coloumn1, coloumn2, coloumn3, coloumn4, coloumn5);
 
         // Kasi nilai
         tableTekananDarah.setItems(tekananDarahList);
@@ -108,35 +74,23 @@ public class TableTekananDarah extends Scene {
         tfTangan.setPromptText("Tangan");
         TextField tfCatatan = new TextField();
         tfCatatan.setPromptText("Catatan");
+        TextField tfTanggal = new TextField();
+        tfTanggal.setPromptText("Tanggal");
 
-        Button btnAdd = new Button("Tambah");
-        btnAdd.setOnAction(v -> {
-            tekananDarahList.add(
-                    new TekananDarah(Integer.parseInt(tfSistolik.getText()), Integer.parseInt(tfDiastolik.getText()), tfTangan.getText(), tfCatatan.getText()));
-            daoTekananDarah.syncData(tekananDarahList);
-        });
+        // Button btnAdd = new Button("Tambah");
+        // btnAdd.setOnAction(v -> {
+        //     tekananDarahList.add(
+        //             new TekananDarah(Integer.parseInt(tfSistolik.getText()), Integer.parseInt(tfDiastolik.getText()), tfTangan.getText(), tfCatatan.getText()));
+        //     daoTekananDarah.syncData(tekananDarahList);
+        // });
 
-=======
-       
-
-        Button tambahButton = new Button("Tambah");
-        tambahButton.setOnAction(event -> {
-            SceneTekananDarah scTekananDarah = new SceneTekananDarah(stage, SceneTekananDarah.getTekananDarahList());
-            stage.setScene(scTekananDarah);
-        });
-
->>>>>>> c3644c56205a812715fa598d2b583cf597bd2a3d
         Button kembaliButton = new Button("Kembali");
         kembaliButton.setOnAction(v -> {
             MainScene mainScene = new MainScene(stage);
             mainScene.show();
         });
 
-<<<<<<< HEAD
         root.getChildren().addAll(titleLabel, tambahButton, kembaliButton, tableTekananDarah);
-=======
-        root.getChildren().addAll(titleLabel, tambahButton, kembaliButton);
->>>>>>> c3644c56205a812715fa598d2b583cf597bd2a3d
 
         setRoot(root);
     }
